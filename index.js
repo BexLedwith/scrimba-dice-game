5c6accac8cb3bdf3ec4fcf1a54eec98c08a855d4
BexLedwith <bex.akeakamai@gmail.com>
2021-06-30 10:45:43 -0400

    js variables for game state

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// Create variables for the game state
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
 
+const message = document.getElementById("message");
+const p1Dice = document.getElementById("player1Dice");
+const p2Dice = document.getElementById("player2Dice");
